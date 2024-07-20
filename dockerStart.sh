7ce2b1da5e75dc8922ce318e6fd97ad642e1f1e9
ALeo75C <za.iz.pr@yandex.kz>
2024-07-20 16:27:05 +0300

    add docker start bash

---
+++ dockerStart.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+container=$(docker ps | gpep 'super_duper_docker_name')
+
+if [ container != "" ]; then 
+    docker stop super_duper_docker_name && docker rm super_duper_docker_name
+fi
+
+docker run -d --name super_duper_docker_name -p 3000:3000 -t infra-hw
